64162f21c49d9d0fcf5c48cb8b45a915926198a4
Nilesh Pathade <nilesh.pathade@enyotalearning.com>
2017-12-27 18:28:37 +0530

    enrol payu

---
+++ lang/en/enrol_payu.php
@@ -0,0 +1,63 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'enrol_payu', language 'en'.
+ *
+ * @package    enrol_payu
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['assignrole'] = 'Assign role';
+$string['businessemail'] = 'Payu business email';
+$string['businessemail_desc'] = 'The email address of your business payu account';
+$string['merchantkey'] = 'Merchant Key';
+$string['merchantkey_desc'] = 'The Merchant key of your business payu account';
+$string['merchantsalt'] = 'Merchant Salt';
+$string['merchantsalt_desc'] = 'The Merchant Salt of your business payu account';
+$string['cost'] = 'Enrol cost';
+$string['costerror'] = 'The enrolment cost is not numeric';
+$string['costorkey'] = 'Please choose one of the following methods of enrolment.';
+$string['currency'] = 'Currency';
+$string['defaultrole'] = 'Default role assignment';
+$string['defaultrole_desc'] = 'Select role which should be assigned to users during payu enrolments';
+$string['enrolenddate'] = 'End date';
+$string['enrolenddate_help'] = 'If enabled, users can be enrolled until this date only.';
+$string['enrolenddaterror'] = 'Enrolment end date cannot be earlier than start date';
+$string['enrolperiod'] = 'Enrolment duration';
+$string['enrolperiod_desc'] = 'Default length of time that the enrolment is valid. If set to zero, the enrolment duration will be unlimited by default.';
+$string['enrolperiod_help'] = 'Length of time that the enrolment is valid, starting with the moment the user is enrolled. If disabled, the enrolment duration will be unlimited.';
+$string['enrolstartdate'] = 'Start date';
+$string['enrolstartdate_help'] = 'If enabled, users can be enrolled from this date onward only.';
+$string['expiredaction'] = 'Enrolment expiry action';
+$string['expiredaction_help'] = 'Select action to carry out when user enrolment expires. Please note that some user data and settings are purged from course during course unenrolment.';
+$string['mailadmins'] = 'Notify admin';
+$string['mailstudents'] = 'Notify students';
+$string['mailteachers'] = 'Notify teachers';
+$string['messageprovider:paypal_enrolment'] = 'payu enrolment messages';
+$string['nocost'] = 'There is no cost associated with enrolling in this course!';
+$string['payu:config'] = 'Configure payu enrol instances';
+$string['payu:manage'] = 'Manage enrolled users';
+$string['payu:unenrol'] = 'Unenrol users from course';
+$string['payu:unenrolself'] = 'Unenrol self from the course';
+$string['paypalaccepted'] = 'payu payments accepted';
+$string['pluginname'] = 'payu';
+$string['pluginname_desc'] = 'The payu module allows you to set up paid courses.  If the cost for any course is zero, then students are not asked to pay for entry.  There is a site-wide cost that you set here as a default for the whole site and then a course setting that you can set for each course individually. The course cost overrides the site cost.';
+$string['sendpaymentbutton'] = 'Send payment via payu';
+$string['status'] = 'Allow payu enrolments';
+$string['status_desc'] = 'Allow users to use payu to enrol into a course by default.';
+$string['unenrolselfconfirm'] = 'Do you really want to unenrol yourself from course "{$a}"?';
---
+++ settings.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * payu enrolments plugin settings and presets.
+ *
+ * @package    enrol_payu
+ * @copyright  2010 Eugene Venter
+ * @author     Eugene Venter - based on code by Petr Skoda and others
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->fulltree) {
+
+    //--- settings ------------------------------------------------------------------------------------------
+    $settings->add(new admin_setting_heading('enrol_payu_settings', '', get_string('pluginname_desc', 'enrol_payu')));
+
+    $settings->add(new admin_setting_configtext('enrol_payu/payubusiness', get_string('businessemail', 'enrol_payu'), get_string('businessemail_desc', 'enrol_payu'), '', PARAM_EMAIL));
+
+	
+	$settings->add(new admin_setting_configtext('enrol_payu/merchantkey', get_string('merchantkey', 'enrol_payu'), get_string('merchantkey_desc', 'enrol_payu'), ''));
+
+	$settings->add(new admin_setting_configtext('enrol_payu/merchantsalt', get_string('merchantsalt', 'enrol_payu'), get_string('merchantsalt_desc', 'enrol_payu'), ''));
+
+	
+    $settings->add(new admin_setting_configcheckbox('enrol_payu/mailstudents', get_string('mailstudents', 'enrol_payu'), '', 0));
+
+    $settings->add(new admin_setting_configcheckbox('enrol_payu/mailteachers', get_string('mailteachers', 'enrol_payu'), '', 0));
+
+    $settings->add(new admin_setting_configcheckbox('enrol_payu/mailadmins', get_string('mailadmins', 'enrol_payu'), '', 0));
+
+    // Note: let's reuse the ext sync constants and strings here, internally it is very similar,
+    //       it describes what should happen when users are not supposed to be enrolled any more.
+    $options = array(
+        ENROL_EXT_REMOVED_KEEP           => get_string('extremovedkeep', 'enrol'),
+        ENROL_EXT_REMOVED_SUSPENDNOROLES => get_string('extremovedsuspendnoroles', 'enrol'),
+        ENROL_EXT_REMOVED_UNENROL        => get_string('extremovedunenrol', 'enrol'),
+    );
+    $settings->add(new admin_setting_configselect('enrol_payu/expiredaction', get_string('expiredaction', 'enrol_payu'), get_string('expiredaction_help', 'enrol_payu'), ENROL_EXT_REMOVED_SUSPENDNOROLES, $options));
+
+    //--- enrol instance defaults ----------------------------------------------------------------------------
+    $settings->add(new admin_setting_heading('enrol_paypal_defaults',
+        get_string('enrolinstancedefaults', 'admin'), get_string('enrolinstancedefaults_desc', 'admin')));
+
+    $options = array(ENROL_INSTANCE_ENABLED  => get_string('yes'),
+                     ENROL_INSTANCE_DISABLED => get_string('no'));
+    $settings->add(new admin_setting_configselect('enrol_payu/status',
+        get_string('status', 'enrol_payu'), get_string('status_desc', 'enrol_payu'), ENROL_INSTANCE_DISABLED, $options));
+
+    $settings->add(new admin_setting_configtext('enrol_payu/cost', get_string('cost', 'enrol_payu'), '', 0, PARAM_FLOAT, 4));
+
+    $paypalcurrencies = enrol_get_plugin('payu')->get_currencies();
+    $settings->add(new admin_setting_configselect('enrol_payu/currency', get_string('currency', 'enrol_payu'), '', 'USD', $paypalcurrencies));
+
+    if (!during_initial_install()) {
+        $options = get_default_enrol_roles(context_system::instance());
+        $student = get_archetype_roles('student');
+        $student = reset($student);
+        $settings->add(new admin_setting_configselect('enrol_payu/roleid',
+            get_string('defaultrole', 'enrol_payu'), get_string('defaultrole_desc', 'enrol_payu'), $student->id, $options));
+    }
+
+    $settings->add(new admin_setting_configduration('enrol_payu/enrolperiod',
+        get_string('enrolperiod', 'enrol_payu'), get_string('enrolperiod_desc', 'enrol_payu'), 0));
+}
